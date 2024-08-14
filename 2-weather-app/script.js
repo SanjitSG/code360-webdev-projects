eefeba3db32636a19dec3945822ad3dc8a922a99
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-08-14 21:05:03 +0530

    weather app completed

--- 2-weather-app/script.js
+++ 2-weather-app/script.js
@@ -1,22 +1,41 @@
 document.getElementById("location-form").addEventListener("submit", getWeather);
 
+const weatherDataDisplay = document.getElementById("weather-data");
+
 const API_KEY = "bfe40e05d9371e69717cc8e8daa9bf71";
 
 async function getWeather(e) {
   e.preventDefault();
   try {
-    const location = document.getElementById("location-input").value;
-    console.log(location);
+    const locationInput = document.getElementById("location-input");
+    const location = locationInput.value;
 
     const fetchData = await fetch(
       "http://api.openweathermap.org/data/2.5/weather?q=" +
         location +
         "&appid=bfe40e05d9371e69717cc8e8daa9bf71&units=metric"
     );
-
     const weatherData = await fetchData.json();
-    console.log(weatherData.name);
+
+    weatherDataDisplay.innerHTML = createUI(weatherData);
+    //clears the location
+    locationInput.value = "";
   } catch (error) {
-    console.log(error);
+    createUI(error);
   }
 }
+
+const createUI = (weatherData) => {
+  if (weatherData.cod === 200) {
+    const name = weatherData.name;
+    const status = weatherData.weather[0].main;
+    const temp = weatherData.main.temp;
+
+    return `<h2>${name}</h2>
+    <p>${status}</p>
+    <p>${temp} °C</p>
+    `;
+  } else {
+    return `<h2>Error: City not found</h2>`;
+  }
+};
