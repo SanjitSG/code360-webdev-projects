4a923ac360c9bfc515d25f33e5959411b53a83fa
Sanjit Gawade <45919581+SanjitSG@users.noreply.github.com>
2024-09-09 23:23:07 +0530

    budget tracker

--- 7-expense-tracker/index.js
+++ 7-expense-tracker/index.js
@@ -1,13 +1,18 @@
 let total_amount = 0;
+const form = document.getElementById("budget-form");
+
+form.addEventListener("submit", calc);
 
 function calc(e) {
-  e.preventdefault();
+  e.preventDefault();
+
   const expense_type = document.getElementById("type").value;
   const amount = document.getElementById("amount").value;
+
   if (expense_type == "income") {
-    total_amount += amount;
+    total_amount += parseFloat(amount);
   } else {
-    total_amount -= amount;
+    total_amount -= parseFloat(amount);
   }
-  console.log(total_amount);
+  document.getElementById("total-budget").textContent = "$" + total_amount.toFixed(2);
 }
